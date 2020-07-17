3d85f32dc4210edae9a98e5131d4a03b3658348b
Shlim45 <jhawranko@gmail.com>
2020-07-16 23:19:10 -0400

    Added 4ed.bat for editing with 4coder

---
+++ misc/4ed.bat
@@ -0,0 +1,2 @@
+@echo off
+start w:\4coder\4ed.exe -f 16 -F
--- misc/shell.bat
+++ misc/shell.bat
@@ -2,7 +2,7 @@
 
 REM
 REM  To run this at startup, use this as your shortcut target:
-REM  %windir%\system32\cmd.exe /k C:\Users\Jon\Desktop\Programming\C\Projects\game_client\misc\shell.bat
+REM  %windir%\system32\cmd.exe /k  C:\Users\Jon\Desktop\Programming\C\Projects\game_client\misc\shell.bat
 REM
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
