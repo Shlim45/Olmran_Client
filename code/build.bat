57cf479c1ccb60998db02752ba7ad77a99be1d90
Shlim45 <jhawranko@gmail.com>
2020-07-18 09:43:52 -0400

    Added compiler and linker options to build.bat

--- code/build.bat
+++ code/build.bat
@@ -1,6 +1,20 @@
 @echo off
 
-mkdir ..\build
+set CommonCompilerFlags=-nologo -fp:fast -GR- -Od -Oi -WX -W4 -wd4100 -wd4005 -FC -Z7
+set CommonLinkerFlags=-incremental:no -opt:ref user32.lib gdi32.lib ws2_32.lib
+
+IF NOT EXIST ..\build mkdir ..\build
 pushd ..\build
-cl -FC -Zi ..\code\win32_olmran.cpp user32.lib gdi32.lib
+
+REM cl -FC -Zi ..\code\win32_olmran.cpp user32.lib gdi32.lib
+
+
+REM 32-bit build
+REM cl %CommonCompilerFlags% ..\code\win32_olmran.cpp /link %CommonLinkerFlags%
+
+REM 64-bit build
+del *.pdb > NUL 2> NUL
+echo WAITING FOR PDB > lock.tmp
+cl %CommonCompilerFlags% ..\code\win32_olmran.cpp -Fmwin32_olmran.map /link %CommonLinkerFlags%
+
 popd
