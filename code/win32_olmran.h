548f5e37dc3031f7bb441d1ae5be5c489d3443e3
Shlim45 <jhawranko@gmail.com>
2020-07-19 10:21:36 -0400

    Added typedefs for (u)int8,16,32,64

--- code/win32_olmran.h
+++ code/win32_olmran.h
@@ -10,6 +10,7 @@
 
 #include <ws2tcpip.h>
 #include <windows.h>
+#include <stdint.h>
 
 #define ID_EDITCHILD 100
 #define HOST_ADDRESS "192.168.1.208"
@@ -19,6 +20,16 @@
 #define local_persist static
 #define internal static
 
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+
 struct win32_socket
 {
     SOCKET sock;
@@ -26,10 +37,10 @@ struct win32_socket
     int status;
 };
 
-bool win32_InitAndConnectSocket(HWND GameOutput);
-void win32_CloseSocket();
+//bool win32_InitAndConnectSocket(HWND GameOutput);
+//void win32_CloseSocket();
 
-global_variable bool running;
+global_variable bool GlobalRunning;
 global_variable win32_socket Socket;
 
 #endif //WIN32_OLMRAN_H
