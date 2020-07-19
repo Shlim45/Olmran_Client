a61374963cbc64bc78c6c67585d4f068059829e6
Shlim45 <jhawranko@gmail.com>
2020-07-19 09:41:18 -0400

    Upgraded 4coder script to launch 4.1 Demo

--- misc/4ed.bat
+++ misc/4ed.bat
@@ -1,4 +1,3 @@
 @echo off
-start C:\Users\Jon\Desktop\4coder\4ed.exe
-REM -f 16 -F
-REM start "C:\Users\Jon\Desktop\4coder 4.1 Demo\4coder\4ed.exe" -f 14 -F
+REM start C:\Users\Jon\Desktop\4coder\4ed.exe
+"C:\Users\Jon\Desktop\4coder 4.1 Demo\4coder\4ed.exe" -f 16
