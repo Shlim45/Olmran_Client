89eea415bf180e297f7140cfa64e13e4c2f6dcb2
Shlim45 <jhawranko@gmail.com>
2020-07-19 12:02:33 -0400

    Added files for platform-independent game code, Updated build.bat

--- code/build.bat
+++ code/build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set CommonCompilerFlags=-nologo -fp:fast -GR- -Od -Oi -WX -W4 -wd4100 -wd4005 -wd4189 -FC -Z7
+set CommonCompilerFlags=-nologo -fp:fast -GR- -Od -Oi -WX -W4 -wd4100 -wd4005 -wd4189 -DOLMRAN_INTERNAL=1 -DOLMRAN_SLOW=1 -DOLMRAN_WIN32=1 -FC -Z7
 set CommonLinkerFlags=-incremental:no -opt:ref user32.lib gdi32.lib ws2_32.lib
 
 IF NOT EXIST ..\build mkdir ..\build
@@ -11,7 +11,10 @@ REM cl %CommonCompilerFlags% ..\code\win32_olmran.cpp /link %CommonLinkerFlags%
 
 REM 64-bit build
 del *.pdb > NUL 2> NUL
+REM Optimization switches /O2
 echo WAITING FOR PDB > lock.tmp
+cl %CommonCompilerFlags% ..\code\olmran.cpp -Fmolmran.map -LD /link -incremental:no -PDB:olmran_%random%.pdb
+del lock.tmp
 cl %CommonCompilerFlags% ..\code\win32_olmran.cpp -Fmwin32_olmran.map /link %CommonLinkerFlags%
 
 popd
---
+++ code/olmran.h
@@ -0,0 +1,18 @@
+/* ========================================================================
+   $File: $
+   $Date: $
+   $Revision: $
+   $Creator: Jonathan Hawranko $
+   $Notice: (C) Copyright 2020 by Jonathan Hawranko. All Rights Reserved. $
+   ======================================================================== */
+#ifndef OLMRAN_H
+#define OLMRAN_H
+
+#include "olmran_platform.h"
+/*
+struct game_memory
+{
+    uint64 PermanentStorageSize;
+    void *PermanentStorage;
+};*/
+#endif //OLMRAN_H
