2a05d47d9e738fda9a3f1794a405f42aea0af1d1
Shlim45 <jhawranko@gmail.com>
2020-07-19 09:44:30 -0400

    Added Socket init and connection, persists globally, and AppendText to GameOutput

--- code/win32_olmran.cpp
+++ code/win32_olmran.cpp
@@ -5,19 +5,25 @@
    $Creator: Jonathan Hawranko $
    $Notice: (C) Copyright 2020 by Jonathan Hawranko. All Rights Reserved. $
    ======================================================================== */
+#include "win32_olmran.h"
 
-#include <ws2tcpip.h>
-#include <windows.h>
-
-#define ID_EDITCHILD 100
-#define HOST_ADDRESS "192.168.1.208"
-#define HOST_PORT 4000
-
-#define global_variable static
-#define local_persist static
-#define internal static
-
-global_variable bool running;
+/* https://stackoverflow.com/questions/16329007/win32-appending-text-to-an-edit-control */
+void win32_AppendText(const HWND GameOutput, TCHAR *newText)
+{
+    // get the current selection
+    DWORD StartPos, EndPos;
+    SendMessage( GameOutput, EM_GETSEL, reinterpret_cast<WPARAM>(&StartPos), reinterpret_cast<WPARAM>(&EndPos) );
+    
+    // move the caret to the end of the text
+    int outLength = GetWindowTextLength( GameOutput );
+    SendMessage( GameOutput, EM_SETSEL, outLength, outLength );
+    
+    // insert the text at the new caret position
+    SendMessage( GameOutput, EM_REPLACESEL, TRUE, reinterpret_cast<LPARAM>(newText) );
+    
+    // restore the previous selection
+    SendMessage( GameOutput, EM_SETSEL, StartPos, EndPos );
+}
 
 LRESULT CALLBACK
 MainWindowCallback(HWND   Window,
@@ -27,7 +33,6 @@ MainWindowCallback(HWND   Window,
 {
     LRESULT Result = 0;
     local_persist HWND GameOutput;
-    local_persist SOCKET sock;
     
     TCHAR OutputBytes[] = TEXT("This is the MUD Client for Olmran\r\n")
         TEXT("There are many like it, but this one is special!\r\n")
@@ -39,76 +44,28 @@ MainWindowCallback(HWND   Window,
         {
             // Create Edit Control
             GameOutput = CreateWindowEx(
-                0, TEXT("EDIT"),   // predefined class 
-                NULL,         // no window title 
-                WS_CHILD | WS_VISIBLE | WS_VSCROLL | 
-                ES_LEFT | ES_MULTILINE | ES_READONLY, 
-                0, 0, 0, 0,   // set size in WM_SIZE message 
-                Window,         // parent window 
-                (HMENU) ID_EDITCHILD,   // edit control ID 
-                (HINSTANCE) GetWindowLongPtr(Window, GWLP_HINSTANCE), 
-                NULL);        // pointer not needed 
+                                        0, TEXT("EDIT"),   // predefined class 
+                                        NULL,         // no window title 
+                                        WS_CHILD | WS_VISIBLE | WS_VSCROLL | 
+                                        ES_LEFT | ES_MULTILINE | ES_READONLY, 
+                                        0, 0, 0, 0,   // set size in WM_SIZE message 
+                                        Window,         // parent window 
+                                        (HMENU) ID_EDITCHILD,   // edit control ID 
+                                        (HINSTANCE) GetWindowLongPtr(Window, GWLP_HINSTANCE), 
+                                        NULL);        // pointer not needed 
             
-            // Initialize WinSock
-            WSAData data;
-            WORD ver = MAKEWORD(2,2);
-            int WSResult = WSAStartup(ver, &data);
-            
-            if (WSResult != 0)
+            if (win32_InitAndConnectSocket(GameOutput))
             {
-                TCHAR ErrorOutput[] = TEXT("Can't start WinSock, Err #\r\n");
-                SendMessage(GameOutput, WM_SETTEXT, 0, (LPARAM) ErrorOutput);
-                break;
+                win32_AppendText(GameOutput, OutputBytes);
             }
             
-            // Create socket
-            sock = socket(AF_INET, SOCK_STREAM, 0);
-            if (sock == INVALID_SOCKET)
-            {
-                TCHAR ErrorOutput[] = TEXT("Can't create socket, Err #\r\n");
-                SendMessage(GameOutput, WM_SETTEXT, 0, (LPARAM) ErrorOutput);
-                break;
-            }
-            
-            // Fill in hint structure
-            sockaddr_in hint;
-            hint.sin_family = AF_INET;
-            hint.sin_port = htons(HOST_PORT);
-            inet_pton(AF_INET, HOST_ADDRESS, &hint.sin_addr);
-            
-            // Connect to server
-            int ConnResult = connect(sock, (sockaddr*) &hint, sizeof(hint), 0);
-            if (ConnResult == SOCKET_ERROR)
-            {
-                TCHAR ErrorOutput[] = TEXT("Can't connect to server, Err #\r\n");
-                SendMessage(GameOutput, WM_SETTEXT, 0, (LPARAM) ErrorOutput);
-                
-                closesocket(sock);
-                WSACleanup();
-                break;
-            }
-            
-            // Add text to the window. 
-            SendMessage(GameOutput, WM_SETTEXT, 0, (LPARAM) OutputBytes); 
+            win32_AppendText(GameOutput, TEXT("\r\n\r\nAppending Test TeXt\r\n"));
         } break; 
         
         case WM_SETFOCUS:
         {
             OutputDebugStringA("WM_SETFOCUS\n\r");
             SetFocus(GameOutput); 
-            
-            
-            // Listen loop
-            char buf[4096];
-            while (true)
-            {
-                ZeroMemory(buf, sizeof(buf));
-                int bytesReceived = recv(sock, buf, sizeof(buf), 0);
-                if (bytesReceived > 0)
-                {
-                    SendMessage(GameOutput, WM_SETTEXT, 0, (LPARAM) buf);
-                }
-            }
         } break; 
         
         case WM_SIZE:
@@ -151,12 +108,59 @@ MainWindowCallback(HWND   Window,
     return Result;
 }
 
+bool win32_InitAndConnectSocket(HWND GameOutput)
+{
+    // Initialize WinSock
+    WSAData data;
+    WORD ver = MAKEWORD(2,2);
+    int WSResult = WSAStartup(ver, &data);
+    
+    if (WSResult != 0)
+    {
+        win32_AppendText(GameOutput, TEXT("Can't start WinSock, Err #\r\n"));
+        return false;
+    }
+    
+    // Create Socket
+    Socket.sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (Socket.sock == INVALID_SOCKET)
+    {
+        win32_AppendText(GameOutput, TEXT("Can't create socket, Err #\r\n"));
+        return false;
+    }
+    
+    // Fill in hint structure
+    Socket.addr.sin_family = AF_INET;
+    Socket.addr.sin_port = htons(HOST_PORT);
+    inet_pton(AF_INET, HOST_ADDRESS, &Socket.addr.sin_addr);
+    
+    // Connect to server
+    if (connect(Socket.sock, (sockaddr*) &Socket.addr, sizeof(Socket.addr)) == SOCKET_ERROR)
+    {
+        win32_AppendText(GameOutput, TEXT("Can't connect to server, Err #\r\n"));
+        
+        win32_CloseSocket();
+        return false;
+    }
+    
+    Socket.status = 1;
+    return true;
+}
+
+void win32_CloseSocket()
+{
+    closesocket(Socket.sock);
+    WSACleanup();
+    Socket.status = 0;
+    //Socket = {};
+}
+
 int CALLBACK
 WinMain(
-HINSTANCE   Instance,
-HINSTANCE   PrevInstance,
-LPSTR       CommandLine,
-int         ShowCode)
+        HINSTANCE   Instance,
+        HINSTANCE   PrevInstance,
+        LPSTR       CommandLine,
+        int         ShowCode)
 {
     WNDCLASS WindowClass = {};
     // TODO(jon):  Check if any of this is needed.
@@ -174,18 +178,18 @@ int         ShowCode)
     {
         HWND WindowHandle =
             CreateWindowEx(
-            0,
-            WindowClass.lpszClassName,
-            "Olmran Client",
-            WS_OVERLAPPEDWINDOW|WS_SIZEBOX|WS_MAXIMIZE|WS_VISIBLE,
-            CW_USEDEFAULT,
-            CW_USEDEFAULT,
-            CW_USEDEFAULT,
-            CW_USEDEFAULT,
-            0,
-            0,
-            Instance,
-            0);
+                           0,
+                           WindowClass.lpszClassName,
+                           "Olmran Client",
+                           WS_OVERLAPPEDWINDOW|WS_SIZEBOX|WS_VISIBLE,
+                           CW_USEDEFAULT,
+                           CW_USEDEFAULT,
+                           CW_USEDEFAULT,
+                           CW_USEDEFAULT,
+                           0,
+                           0,
+                           Instance,
+                           0);
         
         if(WindowHandle)
         {
@@ -202,6 +206,7 @@ int         ShowCode)
                 else
                     break;
             }
+            win32_CloseSocket();
         }
         else
         {
