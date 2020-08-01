f3e68020e9216f05f686455d0ea30fdc12e15c98
Shlim45 <jhawranko@gmail.com>
2020-08-01 10:21:18 -0400

    Start 4coder fullscreen

--- misc/4ed.bat
+++ misc/4ed.bat
@@ -1,3 +1,3 @@
 @echo off
 REM start C:\Users\Jon\Desktop\4coder\4ed.exe
-start "4coder:  Olmran Game Client" "C:\Users\Jon\Desktop\4coder 4.1 Demo\4coder\4ed.exe" -f 16
+start "4coder:  Olmran Game Client" "C:\Users\Jon\Desktop\4coder 4.1 Demo\4coder\4ed.exe" -f 16 -F
