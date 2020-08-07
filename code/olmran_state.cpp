1e0a934e0d304a8652e7e6293d5345da831bfdd3
Shlim45 <jhawranko@gmail.com>
2020-08-06 22:22:32 -0400

    Refactor olmran_state.cpp

--- code/olmran_state.cpp
+++ code/olmran_state.cpp
@@ -10,78 +10,99 @@ InitializeCommandHistory(game_state gState, char *buffer)
 #endif
 
 internal void
+SetCaretToEnd(HWND TextBoxHandle)
+{
+    // NOTE(jon):  When finished, set caret to end of GameInput textbox.
+    int index = GetWindowTextLengthA( TextBoxHandle );
+    SendMessage( TextBoxHandle, EM_SETSEL, (WPARAM)index, (LPARAM)index );
+}
+
+// TODO(jon):  Turn this in to a generic HWND text to buffer function
+internal void
+StoreCurrentCommand()
+{
+    memset(GameState.CommandHistory.CurrentCommand, 0, GameState.CommandHistory.CurrentSize);
+    
+    GetWindowTextA(GameState.GameInput.Window,
+                   GameState.CommandHistory.CurrentCommand,
+                   GameState.CommandHistory.CurrentSize);
+}
+
+internal bool32
+RestorePreviousCommand()
+{
+    bool32 Success = SetWindowTextA(GameState.GameInput.Window,
+                                    GameState.CommandHistory.CurrentCommand);
+    if (Success)
+        memset(GameState.CommandHistory.CurrentCommand, 0, GameState.CommandHistory.CurrentSize);
+    
+    return Success;
+}
+
+internal void
 CycleThroughUserInputHistory(int8 Direction)
 {
-    if (GameState.CommandHistory.CurrentPosition < 0 && Direction > 0)
+    int16 *pos = &GameState.CommandHistory.CurrentPosition;
+    
+    if (*pos < 0 && Direction > 0)
+        StoreCurrentCommand();
+    
+    else if (*pos == 0 && Direction < 0)
     {
-        memset(GameState.CommandHistory.CurrentCommand, 0, GameState.CommandHistory.CurrentSize);
-        
-        GetWindowTextA(GameState.GameInput.Window,
-                       GameState.CommandHistory.CurrentCommand,
-                       GameState.CommandHistory.CurrentSize);
-        
-    }
-    else if (GameState.CommandHistory.CurrentPosition == 0 && Direction < 0)
-    {
-        bool32 Success = SetWindowTextA(GameState.GameInput.Window,
-                                        GameState.CommandHistory.CurrentCommand);
+        bool32 Success = RestorePreviousCommand();
         if (Success)
-        {
-            memset(GameState.CommandHistory.CurrentCommand, 0, GameState.CommandHistory.CurrentSize);
-            
-            GameState.CommandHistory.CurrentPosition--;
-            
-        }
+            --*pos;
     }
     
+    // NOTE(jon):  Cycling upward, thru spots 0-MAX
     if (Direction > 0 
-        && GameState.CommandHistory.CurrentPosition < (GameState.CommandHistory.NumberOfCommands-1))
+        && *pos < (GameState.CommandHistory.NumberOfCommands-1))
     {
         // cycle up
-        char *NewText = GameState.CommandHistory.Commands + ((GameState.CommandHistory.CurrentPosition+1) * GameState.CommandHistory.BufferSize);
+        char *NewText = GameState.CommandHistory.Commands + ((*pos+1) * GameState.CommandHistory.BufferSize);
         
         if (NewText[0] != 0)
         {
-            GameState.CommandHistory.CurrentPosition++;
+            ++*pos;
             
             SetWindowTextA(GameState.GameInput.Window,
-                           GameState.CommandHistory.Commands + (GameState.CommandHistory.CurrentPosition * GameState.CommandHistory.BufferSize));
+                           GameState.CommandHistory.Commands + (*pos * GameState.CommandHistory.BufferSize));
         }
     }
-    else if (Direction < 0 && GameState.CommandHistory.CurrentPosition > -1)
+    // NOTE(jon):  Cycling downward, thru spots MAX-0
+    else if (Direction < 0 && *pos > -1)
     {
         // cycle down
-        GameState.CommandHistory.CurrentPosition--;
+        --*pos;
         
         SetWindowTextA(GameState.GameInput.Window,
-                       GameState.CommandHistory.Commands + (GameState.CommandHistory.CurrentPosition * GameState.CommandHistory.BufferSize));
+                       GameState.CommandHistory.Commands + (*pos * GameState.CommandHistory.BufferSize));
     }
     
-    // Set caret to end
-    int index = GetWindowTextLengthA( GameState.GameInput.Window );
-    SendMessage( GameState.GameInput.Window, EM_SETSEL, (WPARAM)index, (LPARAM)index );
+    SetCaretToEnd( GameState.GameInput.Window );
 }
 
 internal void
 UpdateCommandHistory()
 {
+    uint16 BufferSize = GameState.CommandHistory.BufferSize;
     for (int Slot = GameState.CommandHistory.NumberOfCommands - 1;
          Slot >= 0;
          Slot--)
     {
         // clear this buffer slot
-        memset(GameState.CommandHistory.Commands + (GameState.CommandHistory.BufferSize * Slot),
-               0, GameState.CommandHistory.BufferSize);
+        memset(GameState.CommandHistory.Commands + (BufferSize * Slot),
+               0, BufferSize);
         
         // copy previous slot to this slot
-        strcpy_s(GameState.CommandHistory.Commands + (GameState.CommandHistory.BufferSize * Slot),
-                 GameState.CommandHistory.BufferSize,
-                 GameState.CommandHistory.Commands + (GameState.CommandHistory.BufferSize * (Slot-1)));
+        strcpy_s(GameState.CommandHistory.Commands + (BufferSize * Slot),
+                 BufferSize,
+                 GameState.CommandHistory.Commands + (BufferSize * (Slot-1)));
         
         if (Slot == 0)
         {
             // This copies the CurrentCommand buffer into the Commands[0] buffer.
-            strcpy_s(GameState.CommandHistory.Commands, GameState.CommandHistory.BufferSize,
+            strcpy_s(GameState.CommandHistory.Commands, BufferSize,
                      GameState.CommandHistory.CurrentCommand);
             
             // Clear CurrentCommand buffer
